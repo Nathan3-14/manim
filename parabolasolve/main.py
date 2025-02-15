58592f91ded2a210d40f7a9461cd0eaebf188ce8
nathan3-14 <nathan.watson.172@gmail.com>
2025-02-15 15:56:01 +0000

    Added solving parabola

--- parabolasolve/main.py
+++ parabolasolve/main.py
@@ -1,32 +1,66 @@
 from manim import (
-    Scene, Axes,
-    Write,
+    Scene, Axes, DashedLine, Circle,
+    Write, ReplacementTransform, Create, FadeOut,
     BLUE, RIGHT, LEFT, DOWN, RED, GREEN, 
     Tex, MathTex
 )
 
 class Parabola(Scene):
+    def question_1(self) -> None:
+        given_parabola = self.axes.plot(lambda x: (x**2) - 2*(x) - 3, color=RED)
+        # given_parabola_label = axes.get_graph_label(given_parabola, "y=x^2-x-3", x_val=2)
+        solve_parabola = self.axes.plot(lambda x: (x**2) + (x) - 2, color=BLUE)
+        solve_line = self.axes.plot(lambda x: (-3)*(x) - 1, color=GREEN)
+        
+        given_parabola_equation = MathTex(r"\ x^{2}-2x-3", color=RED)
+        calculation = MathTex(
+            r"x^{2}-2x-3 \\",
+            r"-x^{2}+\ x-2",
+            tex_to_color_map={
+                r"x^{2}-2x-3": RED,
+                r"x^{2}+\ x-2": BLUE
+            }
+        )
+        answer = MathTex(
+            r"-3x-1",
+            color=GREEN
+        )
+        given_parabola_equation.next_to(self.axes, RIGHT)
+        calculation.next_to(self.axes, RIGHT)
+        answer.next_to(calculation, DOWN)
+
+        solve_solution_a = DashedLine(self.axes.coords_to_point(-2, 5), self.axes.coords_to_point(-2, 0))
+        solve_solution_b = DashedLine(self.axes.coords_to_point(1, -4), self.axes.coords_to_point(1, 0))
+        solve_circle_a = Circle(0.25, color=GREEN)
+        solve_circle_b = Circle(0.25, color=GREEN)
+        solve_circle_a.move_to(self.axes.c2p(-2, 0))
+        solve_circle_b.move_to(self.axes.c2p(1, 0))
+        
+        self.play(Write(self.axes))
+        self.play(Write(given_parabola), Write(given_parabola_equation))
+        self.wait(2)
+        self.play(ReplacementTransform(given_parabola_equation, calculation))
+        self.wait(2)
+        self.play(Write(answer))
+        self.wait(2)
+        self.play(Write(solve_line))
+        self.wait(1)
+        self.play(Create(solve_solution_a), Create(solve_solution_b))
+        self.wait(1)
+        self.play(Create(solve_circle_a), Create(solve_circle_b), FadeOut(solve_solution_a), FadeOut(solve_solution_b))
+        self.wait(2)
+        self.play(FadeOut(given_parabola), FadeOut(solve_line))
+        self.wait(1)
+        self.play(Write(solve_parabola))
+        
     def construct(self) -> None:
-        axes = Axes(
+        self.axes = Axes(
             x_range=(-5, 5),
             y_range=(-5, 10),
             x_length=10,
             tips=False
         ).move_to(LEFT)
-        axes.add_coordinates()
+        self.axes.add_coordinates()
         
-        given_parabola = axes.plot(lambda x: (x**2) - 2*(x) + 1, color=RED)
-        # given_parabola_label = axes.get_graph_label(given_parabola, "y=x^2-x-3", x_val=2)
-        solve_parabola = axes.plot(lambda x: (x**2) + 4*(x) + 3, color=BLUE)
-        # solve_parabola_label = axes.get_graph_label(solve_parabola, "y=x^2+x-1", x_val=2)
-        
-        given_parabola_equation = MathTex("x^{2}-2x+1", color=RED)
-        solve_parabola_equation = MathTex("x^{2}+4x+3", color=BLUE)
-        given_parabola_equation.next_to(axes, RIGHT)
-        solve_parabola_equation.next_to(given_parabola_equation, DOWN)
-        
-        self.play(Write(axes))
-        self.play(Write(given_parabola), Write(given_parabola_equation))
-        self.wait(2)
-        self.play(Write(solve_parabola), Write(solve_parabola_equation))
-        self.wait(2)
+        self.question_1()
+        self.wait(5)
