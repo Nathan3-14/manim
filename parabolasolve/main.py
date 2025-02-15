d675735c1537730baab89eebfa16acb784f5ab2a
nathan3-14 <nathan.watson.172@gmail.com>
2025-02-15 16:24:49 +0000

    Fixed bugs, added q2

--- parabolasolve/main.py
+++ parabolasolve/main.py
@@ -36,7 +36,6 @@ class Parabola(Scene):
         solve_circle_a.move_to(self.axes.c2p(-2, 0))
         solve_circle_b.move_to(self.axes.c2p(1, 0))
         
-        self.play(Write(self.axes))
         self.play(Write(given_parabola), Write(given_parabola_equation))
         self.wait(2)
         self.play(ReplacementTransform(given_parabola_equation, calculation))
@@ -52,15 +51,91 @@ class Parabola(Scene):
         self.play(FadeOut(given_parabola), FadeOut(solve_line))
         self.wait(1)
         self.play(Write(solve_parabola))
+        self.wait(3)
+        self.play(FadeOut(solve_circle_a), FadeOut(solve_circle_b), FadeOut(solve_parabola), FadeOut(calculation), FadeOut(answer))
+    
+    def question2(self) -> None:        
+        given_parabola = self.axes.plot(lambda x: (x**2) - 2*(x) - 3, color=RED)
+        # given_parabola_label = axes.get_graph_label(given_parabola, "y=x^2-x-3", x_val=2)
+        solve_parabola = self.axes.plot(lambda x: 2*(x**2) + 3*(x) - 2, color=BLUE)
+        solve_line = self.axes.plot(lambda x: -3.5 * x - 2, color=GREEN)
         
+        given_parabola_equation = MathTex(r"\ x^{2}-2x-3", color=RED)
+        calculation_a = MathTex(
+            r"x^{2}-2x-3 \\",
+            r"-2x^{2}+\ x-2",
+            tex_to_color_map={
+                r"x^{2}-2x-3": RED,
+                r"-2x^{2}+\ x-2": BLUE
+            }
+        )
+        calculation_b = MathTex(
+            r"x^{2}-2x-3 \\",
+            r"-2x^{2}+\ x-2",
+            r"\div 2",
+            tex_to_color_map={
+                r"x^{2}-2x-3": RED,
+                r"-2x^{2}+\ x-2": BLUE
+            }
+        )
+        calculation_c = MathTex(
+            r"x^{2}-2x-3 \\",
+            r"-x^{2}+\frac{1}{2}x-1",
+            tex_to_color_map={
+                r"x^{2}-2x-3": RED,
+                r"-x^{2}+\frac{1}{2}x-1": BLUE
+            }
+        )
+        answer = MathTex(
+            r"-\frac{7}{2}x-2",
+            color=GREEN
+        )
+        given_parabola_equation.next_to(self.axes, RIGHT)
+        calculation_a.next_to(self.axes, RIGHT)
+        calculation_b.next_to(self.axes, RIGHT)
+        calculation_c.next_to(self.axes, RIGHT)
+        answer.next_to(calculation_c, DOWN)
+
+        solve_solution_a = DashedLine(self.axes.coords_to_point(-2, 5), self.axes.coords_to_point(-2, 0))
+        solve_solution_b = DashedLine(self.axes.coords_to_point(0.5, -3.75), self.axes.coords_to_point(0.5, 0))
+        solve_circle_a = Circle(0.25, color=GREEN)
+        solve_circle_b = Circle(0.25, color=GREEN)
+        solve_circle_a.move_to(self.axes.c2p(-2, 0))
+        solve_circle_b.move_to(self.axes.c2p(0.5, 0))
+        
+        self.play(Write(given_parabola), Write(given_parabola_equation))
+        self.wait(2)
+        self.play(ReplacementTransform(given_parabola_equation, calculation_a))
+        self.wait(1)
+        self.play(ReplacementTransform(calculation_a, calculation_b))
+        self.wait(1)
+        self.play(ReplacementTransform(calculation_b, calculation_c))
+        self.wait(2)
+        self.play(Write(answer))
+        self.wait(2)
+        self.play(Write(solve_line))
+        self.wait(1)
+        self.play(Create(solve_solution_a), Create(solve_solution_b))
+        self.wait(1)
+        self.play(Create(solve_circle_a), Create(solve_circle_b), FadeOut(solve_solution_a), FadeOut(solve_solution_b))
+        self.wait(2)
+        self.play(FadeOut(given_parabola), FadeOut(solve_line))
+        self.wait(1)
+        self.play(Write(solve_parabola))
+        self.wait(3)
+        self.play(FadeOut(solve_circle_a), FadeOut(solve_circle_b), FadeOut(solve_parabola), FadeOut(calculation_c), FadeOut(answer))
+        
+    
     def construct(self) -> None:
         self.axes = Axes(
             x_range=(-5, 5),
             y_range=(-5, 10),
-            x_length=10,
+            x_length=8,
             tips=False
         ).move_to(LEFT)
         self.axes.add_coordinates()
+        self.play(Write(self.axes))
         
         self.question_1()
-        self.wait(5)
+        self.wait(2)
+        self.question2()
